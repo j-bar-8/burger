4b3fac8df6e6b7386e1d5994ac906aefa0f8b42c
j-bar-8 <josh.barkat@gmail.com>
2018-08-22 14:39:01 -0400

    routing fixed

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,47 @@
+// DEPENDENCIES AND IMPORTING MODEL
+// ==================================================================
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger");
+
+// ROUTES
+// ==================================================================
+router.get("/", (req, res) => {
+    burger.selectAll((data) => {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", (req,res) => {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], (result) => {
+        res.json({id: result.insertId});
+    });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, (result) => {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// EXPORT ROUTES FOR SERVER.JS
+// ==================================================================
+module.exports = router;
